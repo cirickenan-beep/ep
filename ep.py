881fb80ef6a9c05463173ba13199b1489b589e1e
cirickenan-beep <cirickenan@gmail.com>
2026-06-23 13:35:55 +0200

    Add snow effect to button interaction

--- ep.py
+++ ep.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.title("Ovo je za tb")
 if st.button("Klikni ovde"):
   st.balloons()
+  st.snow()
   st.write("Ma preslatka si ukratko da neduljim  haahh")
   st.write("Voli te tvoj programer")
   
