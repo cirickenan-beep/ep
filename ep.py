910dc4df427c65a504bef69d583cdbe70b4a739a
cirickenan-beep <cirickenan@gmail.com>
2026-06-23 13:29:41 +0200

    Add Streamlit app with title and button interaction
    Fix button callback and correct spelling error.

---
+++ ep.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Ovo je za tb")
+st.button("Klikni ovde")
+if st.button:
+  st.write("Ma preslatka si ukratko da nedulji  haahh")
+  st.writte("Voli te tvoj programer")
+  
