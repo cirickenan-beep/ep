c025a79b5054428369465c478af8b6abb0bfcaa8
cirickenan-beep <cirickenan@gmail.com>
2026-06-23 13:35:25 +0200

    Fix button functionality and balloon display

--- ep.py
+++ ep.py
@@ -1,8 +1,7 @@
 import streamlit as st
 st.title("Ovo je za tb")
-st.button("Klikni ovde")
-if st.button:
-  st.ballons
-  st.write("Ma preslatka si ukratko da nedulji  haahh")
+if st.button("Klikni ovde"):
+  st.balloons()
+  st.write("Ma preslatka si ukratko da neduljim  haahh")
   st.write("Voli te tvoj programer")
   
