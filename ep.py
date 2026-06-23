8c9ff5913a224d61f680efd6afec8978a7e6ca1d
cirickenan-beep <cirickenan@gmail.com>
2026-06-23 13:31:02 +0200

    Fix typo in write function and update button logic

--- ep.py
+++ ep.py
@@ -3,5 +3,5 @@ st.title("Ovo je za tb")
 st.button("Klikni ovde")
 if st.button:
   st.write("Ma preslatka si ukratko da nedulji  haahh")
-  st.writte("Voli te tvoj programer")
+  st.write("Voli te tvoj programer")
   
