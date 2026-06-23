f3b9cf828e9c38a38d8192a2c3c7bd3e7325936e
cirickenan-beep <cirickenan@gmail.com>
2026-06-23 13:32:52 +0200

    Fix button functionality and add balloon effect

--- ep.py
+++ ep.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.title("Ovo je za tb")
 st.button("Klikni ovde")
 if st.button:
+  st.ballons
   st.write("Ma preslatka si ukratko da nedulji  haahh")
   st.write("Voli te tvoj programer")
   
